3f7c582fc3c4b6625642621700a16daf0b73570a
Ricardo <ricardodeveloper568@gmail.com>
2024-01-04 09:31:10 -0500

    feat: implementar patron builder

--- src/main/java/com/comfama/challengeanime/api/dto/ResponseDto.java
+++ src/main/java/com/comfama/challengeanime/api/dto/ResponseDto.java
@@ -1,15 +1,54 @@
 package com.comfama.challengeanime.api.dto;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
 
 @Data
-@Builder
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ResponseDto<T> {
     Integer status;
     String message;
     T data;
+
+    private ResponseDto() {
+        // Constructor privado para forzar el uso del Builder
+    }
+
+    public static <T> Builder<T> builder() {
+        return new Builder<>();
+    }
+
+    public static class Builder<T> {
+        Integer status;
+        String message;
+        T data;
+
+        private Builder() {
+            // Constructor privado del Builder
+        }
+
+        public Builder<T> status(Integer status) {
+            this.status = status;
+            return this;
+        }
+
+        public Builder<T> message(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public Builder<T> data(T data) {
+            this.data = data;
+            return this;
+        }
+
+        public ResponseDto<T> build() {
+            ResponseDto<T> responseDto = new ResponseDto<>();
+            responseDto.setStatus(status);
+            responseDto.setMessage(message);
+            responseDto.setData(data);
+            return responseDto;
+        }
+    }
 }
